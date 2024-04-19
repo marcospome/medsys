2701f89a7170f68c7da7cb79e726ea314b709427
Marcos <mpomeranietz@gmail.com>
2024-04-19 16:06:28 -0300

    fix historial clinico

--- medicalsystem/apps/historialesclinicos/admin.py
+++ medicalsystem/apps/historialesclinicos/admin.py
@@ -3,46 +3,54 @@ from django.utils.html import strip_tags
 from .models import HistorialClinico
 
 class HistorialClinicoAdmin(admin.ModelAdmin):
+    # Lista de campos a mostrar en la lista de registros
     list_display = ('get_fecha_display', 'socio', 'usuario', 'observacion_plain')
 
+
+
+# ---------------------------- METODOS/LOGICA DEL ADMISTRADOR DE DJANGO PARA EL MODULO DE HISTORIALES CLINICOS ----------------------------
     def save_model(self, request, obj, form, change):
+        # Asigna el usuario actual como el responsable del historial si es un registro nuevo, si se edita no cuenta de igual manera no se puede editar si no es superadmin.
         if not obj.pk:
-            # Si el objeto no tiene clave primaria (es decir, es un nuevo registro)
             obj.usuario = request.user
         super().save_model(request, obj, form, change)
 
-    # Formatea la fecha aaaa/mm/dd a dd/mm/aaaa para visualizarla de esta manera.
+    # Formatea la fecha a formato personalizado para mostrarla en la lista de registros
     def get_fecha_display(self, obj):
-        return obj.fecha.strftime('%d %B %Y')  # Formato de fecha personalizado
+        return obj.fecha.strftime('%d %B %Y')
     get_fecha_display.short_description = 'Fecha de carga'
 
+    # Remueve las etiquetas HTML de la observación para mostrarla sin formato en la lista de registros
     def observacion_plain(self, obj):
         return strip_tags(obj.observacion)
     observacion_plain.short_description = 'Observación'
 
-    def observacion_view(self, obj):
-        return self.observacion_plain(obj)
-    observacion_view.short_description = 'Observación'
+    # Define alias para los métodos de observación
+    observacion_view = observacion_plain
+    observacion_change = observacion_plain
 
-    def observacion_change(self, obj):
-        return obj.observacion
-    observacion_change.short_description = 'Observación'
-
+    # Obtiene los campos que se mostrarán en el formulario
     def get_fields(self, request, obj=None):
         fields = super().get_fields(request, obj)
         if obj:
-            fields = list(fields)
-            # Reemplaza el campo observacion con observacion_plain
-            fields[fields.index('observacion')] = 'observacion_plain'
+            # Añade el campo 'usuario' si no está presente
+            if 'usuario' not in fields:
+                fields.append('usuario')
         return fields
 
-    # Añade el campo fecha a los campos a mostrar en la vista de detalle
+    # Define los campos y su orden en la vista de detalle
     def get_fieldsets(self, request, obj=None):
         fieldsets = super().get_fieldsets(request, obj)
         if obj:
             fields = list(fieldsets[0][1]['fields'])
             fields.insert(0, 'fecha')
+            # Reemplaza el campo 'observacion' con 'observacion_plain'
+            fields[fields.index('observacion')] = 'observacion_plain'
             fieldsets[0][1]['fields'] = tuple(fields)
         return fieldsets
+    
+    # ---------------------------- METODOS/LOGICA DEL ADMISTRADOR DE DJANGO PARA EL MODULO DE HISTORIALES CLINICOS ----------------------------
 
+
+# Registra el modelo HistorialClinico junto con su administrador personalizado
 admin.site.register(HistorialClinico, HistorialClinicoAdmin)
--- medicalsystem/apps/historialesclinicos/models.py
+++ medicalsystem/apps/historialesclinicos/models.py
@@ -3,13 +3,16 @@ from apps.socio.models import Paciente
 from django.contrib.auth.models import User
 from ckeditor.fields import RichTextField  # Importa RichTextField desde ckeditor
 
-# Create your models here.
+# ------- Modelo historiales clinicos -------
+
 class HistorialClinico(models.Model):
     
-    fecha = models.DateField(verbose_name='Fecha de carga', auto_now_add=True, editable=False)
-    observacion = RichTextField()
-    socio = models.ForeignKey(Paciente, on_delete=models.CASCADE)
-    usuario = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, verbose_name='Medico Responsable', editable=False)
+    fecha = models.DateField(verbose_name='Fecha de carga', auto_now_add=True) # Fecha en la que se carga el historial | se autoasigna como un getdate()
+    observacion = RichTextField() # Campo de texto estilo word para qué el medico pueda describir todo el detalle del paciente.
+    socio = models.ForeignKey(Paciente, on_delete=models.CASCADE) # Socio/Paciente al cual corresponde el historial.
+    usuario = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, verbose_name='Medico Responsable', editable=False) # Usuario que cargo el historial, debe ser un medico o superadmin ya que solo ellos tienen permiso para cargar nuevos historiales.
+
+# ------- Función para traer x campos del modelo historiales clinicos -------
 
     def __str__(self):  
         return f"{self.fecha}, {self.socio}, {self.usuario}"
