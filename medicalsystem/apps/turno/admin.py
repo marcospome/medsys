2a0369d5a5117949b435bf1cc214044e6fdbc8eb
Marcos <mpomeranietz@gmail.com>
2024-04-29 01:59:04 -0300

    fix turnos (falta logica que los medicos solo vean sus turnos y no puedan ver los inactivos) tambien que si se edita el campo observación un turno no pinche por el horario ocupado

--- medicalsystem/apps/turno/admin.py
+++ medicalsystem/apps/turno/admin.py
@@ -1,96 +1,48 @@
-from django import forms  # Importa el módulo de formularios de Django
-from django.contrib import admin  # Importa el módulo de administración de Django
-from django.contrib.auth.models import Group  # Importa el modelo de grupos de usuarios de Django
-from django.core.exceptions import ValidationError  # Importa la excepción de validación de Django
-from django.utils.translation import gettext_lazy as _  # Importa la función de traducción de Django
-from .models import Turno  # Importa el modelo Turno desde el archivo models.py del mismo directorio
-import datetime  # Importa el módulo datetime de Python estándar
+import datetime
+from django.contrib import admin
+from django.contrib.auth.models import User
+from django import forms
+from .models import Turno
 
-# Define un formulario personalizado para el modelo Turno
-class TurnoAdminForm(forms.ModelForm):
+class TurnoForm(forms.ModelForm):
     class Meta:
-        model = Turno  # Especifica el modelo asociado al formulario
-        fields = '__all__'  # Define que se deben incluir todos los campos del modelo
+        model = Turno
+        fields = '__all__'
 
-    # Define mensajes de error personalizados
-    error_messages = {
-        'horario': {
-            'invalid_choice': _("Ya existe un turno activo para este médico en la misma fecha y hora. Por favor, seleccione otro horario, otra fecha o consulte con otro médico."),
-        },
-    }
-
-    # Método para limpiar y validar el campo 'fecha'
-    def clean_fecha(self):
-        fecha = self.cleaned_data['fecha']
-        return fecha.strftime('%Y-%m-%d')  # Convierte la fecha al formato YYYY-MM-DD
-
-    # Método para realizar validaciones personalizadas en todos los campos del formulario
-    def clean(self):
-        cleaned_data = super().clean()
-        fecha = cleaned_data.get("fecha")
-        horario = cleaned_data.get("horario")
-        medico = cleaned_data.get("usuario")
-
-        # Verifica si se está editando y se cambia el estado activo/inactivo
-        if 'activo' in self.changed_data:
-            # Si se está cambiando a inactivo, no es necesario realizar la validación
-            if not cleaned_data['activo']:
-                return cleaned_data
-
-        # Verifica si hay un turno activo para el mismo médico en la misma fecha y hora
-        if fecha and horario and medico:
-            turnos_activos_existen = Turno.objects.filter(fecha=fecha, horario=horario, usuario=medico, activo=True).exists()
-            if turnos_activos_existen:
-                raise ValidationError(
-                    self.error_messages['horario']['invalid_choice'],
-                    code='invalid_choice',
-                    params={'value': horario},
-                )
-
-        return cleaned_data
-
-# Define la clase TurnoAdmin para personalizar la interfaz de administración del modelo Turno
 class TurnoAdmin(admin.ModelAdmin):
-    # Especifica los campos a mostrar en la lista de turnos en el panel de administración
-    list_display = ['fecha', 'horario', 'socio', 'usuario', 'responsable_de_carga', 'activo']
-    # Asocia el formulario personalizado al modelo Turno en el panel de administración
-    form = TurnoAdminForm
-    list_filter = ['socio', 'activo']
+    form = TurnoForm
+    list_display = ('fecha', 'horario', 'socio', 'usuario', 'activo', 'responsable_de_carga')
+    list_filter = ('fecha', 'horario', 'activo', 'usuario')
+    search_fields = ('fecha', 'socio__nombre', 'usuario__username')
 
-    # Método para traer unicamente los usuarios con rol Medico en la lista de "Medico Asignado".
+    def save_model(self, request, obj, form, change):
+        # Asignar el usuario autenticado como responsable de carga
+        obj.responsable_de_carga = request.user
+        super().save_model(request, obj, form, change)
+
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        if db_field.name == 'usuario':
-            grupo_medico = Group.objects.get(name='Medico')
-            kwargs['queryset'] = grupo_medico.user_set.all()
+        if db_field.name == "usuario":
+            kwargs["queryset"] = User.objects.filter(groups__name='Medico')
         return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
-    # Método para que limpie la lista de horarios y saque los no disponibles.
     def formfield_for_choice_field(self, db_field, request, **kwargs):
-        if db_field.name == 'horario':
-            medico_seleccionado = request.POST.get('usuario') or request.GET.get('usuario')
-            if medico_seleccionado:
-                fecha_seleccionada = request.POST.get('fecha') or request.GET.get('fecha')
-                if fecha_seleccionada:
-                    fecha_seleccionada = datetime.datetime.strptime(fecha_seleccionada, '%d/%m/%Y').strftime('%Y-%m-%d')
-                    horarios_ocupados = Turno.objects.filter(fecha=fecha_seleccionada, usuario=medico_seleccionado).values_list('horario', flat=True)
-                    horarios_disponibles = [choice[0] for choice in Turno.HORARIOS if choice[0] not in horarios_ocupados]
-                    if '0' not in horarios_disponibles:
-                        horarios_disponibles.append('0')
-                    kwargs['choices'] = [(choice[0], choice[1]) for choice in Turno.HORARIOS if choice[0] in horarios_disponibles]
-        return super().formfield_for_choice_field(db_field, request, **kwargs)
+        formfield = super().formfield_for_choice_field(db_field, request, **kwargs)
+        if db_field.name == "horario" and request.POST.get('activo', None):  # Verificar si 'activo' está presente en la solicitud POST
+            # Obtener la fecha seleccionada del formulario
+            fecha_seleccionada = request.POST.get('fecha') or request.GET.get('fecha')
+            if fecha_seleccionada:
+                # Procesar la fecha al formato AAAA-MM-DD
+                try:
+                    fecha_formateada = datetime.datetime.strptime(fecha_seleccionada, '%d/%m/%Y').strftime('%Y-%m-%d')
+                    # Obtener los horarios ocupados para la fecha seleccionada
+                    horarios_ocupados = Turno.objects.filter(fecha=fecha_formateada, activo=True).values_list('horario', flat=True)
+                    # Filtrar los horarios disponibles
+                    formfield.choices = [(key, value) for key, value in Turno.HORARIOS if key not in horarios_ocupados]
+                except ValueError:
+                    pass
+        return formfield
+    
 
-    # Método para que cada medico vea unicamente los turnos que tiene asignados a el mismo.
-    def get_queryset(self, request):
-        qs = super().get_queryset(request)
-        if request.user.groups.filter(name='Medico').exists():
-            return qs.filter(usuario=request.user)
-        return qs
 
-    # Método para guardar el turno con el usuario autenticado como responsable.
-    def save_model(self, request, obj, form, change):
-        if not obj.responsable_de_carga:
-            obj.responsable_de_carga = request.user
-        obj.save()
 
-# Registra el modelo Turno y la clase TurnoAdmin en el panel de administración de Django
 admin.site.register(Turno, TurnoAdmin)
--- medicalsystem/apps/turno/models.py
+++ medicalsystem/apps/turno/models.py
@@ -1,22 +1,21 @@
 from django.contrib.auth.models import User
 from django.db import models
 from apps.socio.models import Paciente
-from django.db.models.signals import pre_save
-from django.dispatch import receiver
-from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 
 class Turno(models.Model):
     HORARIOS = (
         ('0', '8:00 - 9:00'),
         ('1', '9:00 - 10:00'),
-        ('3', '10:00 - 11:00'),
-        ('4', '11:00 - 12:00'),
-        ('5', '12:00 - 13:00'),
-        ('6', '13:00 - 14:00'),
-        ('7', '14:00 - 15:00'),
-        ('8', '15:00 - 16:00'),
-        ('9', '16:00 - 17:00'),
-        ('10', '17:00 - 18:00')
+        ('2', '10:00 - 11:00'),
+        ('3', '11:00 - 12:00'),
+        ('4', '12:00 - 13:00'),
+        ('5', '13:00 - 14:00'),
+        ('6', '14:00 - 15:00'),
+        ('7', '15:00 - 16:00'),
+        ('8', '16:00 - 17:00'),
+        ('9', '17:00 - 18:00'),
+        ('10', '18:00 - 19:00')
     )
 
     fecha = models.DateField(verbose_name='Fecha del turno')
@@ -24,13 +23,26 @@ class Turno(models.Model):
     observacion = models.CharField(max_length=300, blank=True)
     activo = models.BooleanField(default=True, verbose_name='¿Turno Activo?')
     socio = models.ForeignKey(Paciente, on_delete=models.CASCADE)
-    usuario = models.ForeignKey(User, related_name='turnos_asignados', on_delete=models.SET_NULL, blank=False, null=True, verbose_name='Medico asignado')
+    usuario = models.ForeignKey(User, related_name='turnos_asignados', on_delete=models.SET_NULL, blank=False, null=True, verbose_name='Médico asignado')
     responsable_de_carga = models.ForeignKey(User, related_name='turnos_responsable', on_delete=models.SET_NULL, null=True, editable=False)
 
     def __str__(self):
         return f'Turno de {self.socio} - {self.fecha} - {dict(self.HORARIOS)[self.horario]}'
 
-@receiver(pre_save, sender=Turno)
-def asignar_responsable_de_carga(sender, instance, **kwargs):
-    if not instance.responsable_de_carga:
-        instance.responsable_de_carga = get_user_model().objects.get(id=instance.usuario.id)
+    def clean(self):
+        # Validar que no exista otro turno para la misma fecha, horario y médico
+        if self.activo and Turno.objects.filter(fecha=self.fecha, horario=self.horario, usuario=self.usuario, activo=True).exists():
+            raise ValidationError('Ya existe un turno activo para este médico en este horario y fecha.')
+
+    def save(self, *args, **kwargs):
+        # Validar si el turno está siendo creado o modificado
+        if not self.pk or (self.pk and not Turno.objects.filter(pk=self.pk, activo=True).exists()):
+            self.full_clean()  # Ejecutar la validación solo si el turno es nuevo o está inactivo
+        super().save(*args, **kwargs)
+
+    def cambiar_estado(self):
+        self.activo = not self.activo
+        self.save()
+
+    class Meta:
+        verbose_name_plural = 'Turnos'
