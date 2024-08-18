6555ea6553efc591eda836b715d0159a971081b8
Marcos <mpomeranietz@gmail.com>
2024-08-18 20:16:37 -0300

    Migración de los datos de PRD

--- medicalsystem/apps/historialesclinicos/models.py
+++ medicalsystem/apps/historialesclinicos/models.py
@@ -8,11 +8,11 @@ from django.contrib.auth.models import User
 class HistorialClinico(models.Model):
     area = models.ForeignKey(Area, on_delete=models.CASCADE) # Area la cual corresponde el historial
     motivo = models.CharField(verbose_name='Motivo de consulta', max_length=500)
-    antecedentefamiliar = models.CharField(verbose_name='Antecedentes Familiares', max_length=10000, blank=True)
-    enfermedad = models.CharField(verbose_name='Enfermedad Actual', max_length=10000, blank=True)
-    indicacion = models.CharField(verbose_name='Indicacion', max_length=10000, blank=True)
-    problema = models.CharField(verbose_name='Problema', max_length=10000, blank=True)
-    Detalle = models.CharField(verbose_name='Detalle', max_length=10000, blank=True)
+    antecedentefamiliar = models.CharField(verbose_name='Antecedentes Familiares', max_length=10000, blank=True, null=True)
+    enfermedad = models.CharField(verbose_name='Enfermedad Actual', max_length=10000, blank=True, null=True)
+    indicacion = models.CharField(verbose_name='Indicacion', max_length=10000, blank=True, null=True)
+    problema = models.CharField(verbose_name='Problema', max_length=10000, blank=True, null=True)
+    Detalle = models.CharField(verbose_name='Detalle', max_length=10000, blank=True, null=True)
     fecha = models.DateField(verbose_name='Fecha de carga', auto_now_add=True) # Fecha en la que se carga el historial | se autoasigna como un getdate()
     socio = models.ForeignKey(Paciente, on_delete=models.CASCADE) # Socio/Paciente al cual corresponde el historial.
     usuario = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, verbose_name='Medico Responsable') # Usuario que cargo el historial, debe ser un medico o superadmin ya que solo ellos tienen permiso para cargar nuevos historiales.
