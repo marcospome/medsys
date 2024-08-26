44b8933203cb70209ff4d8322811528d28355c10
Marcos <mpomeranietz@gmail.com>
2024-08-26 20:36:21 -0300

    fix visualización historiales

--- medicalsystem/apps/socio/views.py
+++ medicalsystem/apps/socio/views.py
@@ -3,7 +3,6 @@ from .models import Paciente
 from apps.turno.models import Turno
 from apps.base.models import Area
 from apps.historialesclinicos.models import HistorialClinico
-from django.contrib.auth.models import Group
 
 class PacienteDetailView(DetailView):
     model = Paciente
@@ -11,26 +10,21 @@ class PacienteDetailView(DetailView):
     context_object_name = 'paciente'
 
     def get_context_data(self, **kwargs):
-        areas = Area.objects.all()
         context = super().get_context_data(**kwargs)
         paciente = self.get_object()
-        user = self.request.user
 
-        # Definir roles por área
-        roles_por_area = {
-            'PS': 'Psiquiatría',
-            'CG': 'Medico',
-            'OD': 'Odontología',
-            'OT': 'Medico'
-        }
+        # Filtrar los historiales clínicos del paciente según las áreas a las que pertenece el usuario
+        user_areas = self.request.user.areas.all()
+        historiales_filtrados = HistorialClinico.objects.filter(area__in=user_areas, socio=paciente)
 
-        # Filtrar historiales por área y verificar permisos
-        historiales_filtrados = []
-        for historial in HistorialClinico.objects.filter(socio=paciente):
-            rol_requerido = roles_por_area.get(historial.area, 'Medico')
-            if user.groups.filter(name=rol_requerido).exists():
-                historiales_filtrados.append(historial)
+        # Verificar si hay historiales clínicos disponibles o acceso
+        if not historiales_filtrados.exists():
+            context['historiales_msg'] = "El paciente no posee historia clínica o no tiene acceso a visualizarla."
+        else:
+            context['historiales'] = historiales_filtrados
 
-        context['turnos'] = Turno.objects.filter(socio=paciente)
-        context['historiales'] = historiales_filtrados
+        # Pasar los turnos del paciente al contexto
+        turnos = Turno.objects.filter(socio=paciente)
+        context['turnos'] = turnos
+
         return context
